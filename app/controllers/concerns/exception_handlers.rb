82be967a2d50ba3c99e50ab3233d2cbe42dd16be
Herbert Kagumba <habukagumba@gmail.com>
2016-07-11 11:15:30 +0300

    fix(user): fix user not found error and logout logic
    Fix the user not found error and refactor logout logic
    
    [#126017539]

--- app/controllers/concerns/exception_handlers.rb
+++ app/controllers/concerns/exception_handlers.rb
@@ -5,15 +5,14 @@ module ExceptionHandlers
   class AccessDeniedError < StandardError; end
   class NotAuthenticatedError < StandardError; end
   class ExpiredSignatureError < StandardError; end
+  class UserNotFoundError < StandardError; end
 
   included do
     rescue_from ExceptionHandlers::AccessDeniedError, with: :access_denied
     rescue_from ExceptionHandlers::NotAuthenticatedError, with: :access_denied
     rescue_from ExceptionHandlers::ExpiredSignatureError, with: :access_denied
-
-    rescue_from ActiveRecord::RecordNotFound do |e|
-      render_json({ errors: e.message }, :not_found)
-    end
+    rescue_from ActiveRecord::RecordNotFound, with: :user_not_found
+    rescue_from ExceptionHandlers::UserNotFoundError, with: :user_not_found
 
     rescue_from ActiveRecord::RecordInvalid do |e|
       render_json({ errors: e.message }, :unprocessable_entity)
@@ -28,5 +27,9 @@ module ExceptionHandlers
 
   def access_denied(e)
     render_json({ errors: e.message }, :unauthorized)
+  end
+
+  def user_not_found(e)
+    render_json({ errors: e.message }, :not_found)
   end
 end
